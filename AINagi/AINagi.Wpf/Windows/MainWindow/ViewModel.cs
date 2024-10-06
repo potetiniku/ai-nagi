fca986f9197ba1b78b76af84bae0e29d5e528030
potetiniku <potetiniku@gmail.com>
2024-10-06 11:41:37 +0900

    UIを実装

--- AINagi/AINagi.Wpf/Windows/MainWindow/ViewModel.cs
+++ AINagi/AINagi.Wpf/Windows/MainWindow/ViewModel.cs
@@ -3,12 +3,21 @@ using AINagi.Model.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NAudio.Wave;
+using Reactive.Bindings;
 
 namespace AINagi.Wpf.Windows.MainWindow;
 
 public partial class ViewModel(MainWindowService service) : ObservableObject
 {
+	public ReactiveProperty<string> Prompt { get; } = new();
+	public ReactiveProperty<string> Answer { get; } = new();
+
 	[RelayCommand]
+	private async Task SendMessage()
+	{
+
+	}
+
 	private async Task GenerateVoice()
 	{
 		await PlayMp3Audio(await service.TextToSpeech("あらゆる現実をすべて自分の方へ捻じ曲げたのだ。"));
